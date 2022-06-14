b7fb3d1667872935701105ebfda0bc5bf142e822
VallenBee <csplus2010.vb@gmail.com>
2022-06-14 23:34:38 +0300

    add some

--- bin/lessons13.dart
+++ bin/lessons13.dart
@@ -1,11 +1,14 @@
 // RECURSIVES FUNCTIONS
-void main(List<String> args) {}
+void main(List<String> args) {
+  List<int> myList = [10, 20, 30, 5, 3, 2];
+  print(addFunction(myList));
+}
 
 int addFunction(List<int> myList) {
-  print(myList);
+  print(' inside = $myList');
   if (myList.length <= 1) {
     return myList[0];
   } else {
-    return myList[0] + addFunction(myList.sublist(1));
+    return myList[0] = addFunction(myList.sublist(1));
   }
 }
