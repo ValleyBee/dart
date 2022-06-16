49fa2bc0faabf77ae2b65d3909c10f3ea929cd26
VallenBee <csplus2010.vb@gmail.com>
2022-06-16 21:39:53 +0300

    add some

--- bin/calc.dart
+++ bin/calc.dart
@@ -1,15 +1,20 @@
+// Rule of importation Libs
+// 1st import Core lib, dart:
+// 2ns import Packages lib, package:
+// 3rd import Own lib
+
 import 'src/calc_lib.dart';
 
 /* 
-
 import 'src/calc_lib copy.dart';   Import Error,
-
 method is already defined in the other libraries. 
 to fix it we can give an alias name, use operator 'as' see next line:
-
 */
 
 import 'src/calc_lib_private.dart' as newcalclib;
+
+// import 'src/calc_lib_private.dart' as newcalclib show add,sub;   SHOW only assign methods
+// import 'src/calc_lib_private.dart' as newcalclib hide div,mul;   HIDE
 
 void main(List<String> args) {
 // methods from own lib calc_dart
