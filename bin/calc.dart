9bad2e19b1a1fe1b412f3db6b0cfbc5dadec0714
VallenBee <csplus2010.vb@gmail.com>
2022-06-16 21:10:19 +0300

    add some

--- bin/calc.dart
+++ bin/calc.dart
@@ -11,8 +11,6 @@ to fix it we can give an alias name, use operator 'as' see next line:
 
 import 'src/calc_lib_new.dart' as newcalclib;
 
-// using own lib
-
 void main(List<String> args) {
 // methods from own lib calc_dart
   print(add(2.1, 2));
@@ -21,6 +19,6 @@ void main(List<String> args) {
   print(mul(2, 2));
   print(powMy(2.1, 2));
 
-// methods from own lib calc_dart
+// methods from own lib  calc_lib_new
   print(newcalclib.add(2, 2));
 }
