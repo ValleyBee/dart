d98d5a2b331c6f0bc8b6d35fb0629181bb33e31c
VallenBee <csplus2010.vb@gmail.com>
2022-06-19 21:12:36 +0300

    add some

--- bin/animalsTest.dart
+++ bin/animalsTest.dart
@@ -1,10 +1,17 @@
 import 'animal.dart';
 import 'cat.dart';
+import 'dog.dart';
 
 void main(List<String> args) {
   List<Animal> animal = [];
   animal.add(Cat('kitty', 1));
   animal.add(Cat('Zuzzy', 2));
+  animal.add(Dog('Wolf', 3));
+  animal.add(Dog('Sharp', 4));
 
-  animal[0].helloMaster();
+  animal.forEach((element) {
+    element.helloMaster();
+  });
+
+  //print(list);
 }
--- bin/dog.dart
+++ bin/dog.dart
@@ -1,7 +1,10 @@
-import 'Animal.dart';
+import 'animal.dart';
 
 class Dog extends Animal {
   Dog(String name, int age) : super(name, age);
 
-  //@override
+  @override
+  void helloMaster() {
+    print('animal: the dog, name: $name, says Hello,Master!');
+  }
 }
