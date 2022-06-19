e63c0f1bd6caa07048434b2d78b6322e95e26dee
VallenBee <csplus2010.vb@gmail.com>
2022-06-19 20:08:25 +0300

    add some

--- bin/Animal.dart
+++ bin/Animal.dart
@@ -3,7 +3,7 @@ class Animal {
   int? age;
   Animal({this.name, this.age});
 
-  void helloMaster(Animal animanl) {
+  void helloMaster(Animal animal) {
     print("animal $name says Hello,Master!!");
   }
 }
