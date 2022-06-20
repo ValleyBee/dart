bd7710c75007d252ef3cbd1fcab88e4da9894827
VallenBee <csplus2010.vb@gmail.com>
2022-06-20 11:44:03 +0300

    add some

--- bin/lesson7.dart
+++ bin/lesson7.dart
@@ -1,3 +1,4 @@
+// SWITCH
 import 'dart:io';
 
 void main(List<String> args) {
@@ -5,47 +6,43 @@ void main(List<String> args) {
 
   // String? s = stdin.readLineSync();
   var s = stdin.readLineSync();
-  
-  if (s !=null) {
 
+  if (s != null) {
     if (int.tryParse(s) == null) {
-       print('Only Integer is allowed $s'); 
-    } else { 
-      int n = int.parse(s); 
-      if (n.isNegative) {
-      print('number is Negativ');
+      print('Only Integer is allowed $s');
     } else {
-      print('your number: $n'); 
-        }
+      int n = int.parse(s);
+      if (n.isNegative) {
+        print('number is Negativ');
+      } else {
+        print('your number: $n');
       }
+    }
   }
 
   print('input a String: one,two or three');
 
- var str = stdin.readLineSync();
+  var str = stdin.readLineSync();
 
- void sw4 (String? s) { 
-  switch(s) {
-    case 'one':
-      print('one-one');
-      continue label1;
-    case 'two':
-      print('two-two');
-      continue label1;
-    case 'three':
-      print('three-three');
-      continue label1;
-    label1:
-    case 'quit':
-      print('going out'); 
-    break;
-    default:
-      print('unknown');
-  }
+  void sw4(String? s) {
+    switch (s) {
+      case 'one':
+        print('one-one');
+        continue label1;
+      case 'two':
+        print('two-two');
+        continue label1;
+      case 'three':
+        print('three-three');
+        continue label1;
+      label1:
+      case 'quit':
+        print('going out');
+        break;
+      default:
+        print('unknown');
+    }
   }
 
   sw4(str);
-
-
-
 }
