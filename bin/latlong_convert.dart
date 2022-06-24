8574a976b0b0bad4da23635ea43974474cb6fee6
VallenBee <csplus2010.vb@gmail.com>
2022-06-24 13:27:38 +0300

    create extension for latlong

--- bin/latlong_convert.dart
+++ bin/latlong_convert.dart
@@ -3,17 +3,17 @@ import 'dart:convert';
 import 'package:latlong2/latlong.dart';
 
 extension JsonPosition on LatLng {
-  Map<String, double> toJson() => {'latitude': latitude, 'longitude': longitude};
+  Map<String, double> _toJson() => {'latitude': latitude, 'longitude': longitude};
 }
 
 void main(List<String> args) {
   // Creating an instance of class LatLng which was extended
-  LatLng position = new LatLng(59.3333, 34.2222);
-  Map json = position.toJson();
+  LatLng position = new LatLng(49.8397, 24.0297);
+  Map json = position._toJson(); // {latitude: 49.8397, longitude: 24.0297}
 
   // same as above. Creating an instance of extension JsonPosition call "constuctor" of LatLng
-  Map json2 = JsonPosition(new LatLng(59.3333, 34.3333)).toJson();
+  Map json2 = JsonPosition(new LatLng(49.8397, 24.0297))._toJson();
 
-  print(jsonEncode(json));
+  print(jsonEncode(json)); // {"latitude":49.8397,"longitude":24.0297}  JSON FORMAT
   print(jsonEncode(json2));
 }
