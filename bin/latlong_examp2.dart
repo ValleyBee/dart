4ee3fd14046d44606b49dbbe8e5eca6958e44101
VallenBee <csplus2010.vb@gmail.com>
2022-06-24 19:28:14 +0300

    create extension for latlong

--- bin/latlong_examp2.dart
+++ bin/latlong_examp2.dart
@@ -25,13 +25,14 @@ extension JsonPosition on LatLng {
 // 49.8397° N, 24.0297° E
 void main(List<String> args) {
   LatLng originPosition = new LatLng(23.333, 44.333);
-  //originPosition._fromJson('latitude': 49.8397,'longitude': 333);
-  json pos = originPosition._toJson(); // Map({})
-  print('originPosition as Object: $originPosition');
-  print('local var pos before change: $pos'); // Map({})
-  pos['latitude'] = 77.777; // change latitude of
+  json pos =
+      originPosition._toJson(); // Objec to Map({}) format {latitude: 23.333, longitude: 44.333}
+  print('originPosition as Object: $originPosition'); // LatLng(latitude:23.333, longitude:44.333)
+  print('local var pos before change: $pos');
+  pos['latitude'] = 77.777; // change latitude
   print('var pos after change: $pos'); // Map({})
   print('originPosition as Object before change: $originPosition');
+
   originPosition.fromJsonChange(pos); // change originalPosition of latitude
   print('originPosition as Object after change: $originPosition');
 
