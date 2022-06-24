5c8b8db3d8e092947e20a721295fb066228ad119
VallenBee <csplus2010.vb@gmail.com>
2022-06-24 18:32:46 +0300

    add some

--- bin/latlong_convert.dart
+++ bin/latlong_convert.dart
@@ -9,11 +9,12 @@ extension JsonPosition on LatLng {
 void main(List<String> args) {
   // Creating an instance of class LatLng which was extended
   LatLng position = new LatLng(49.8397, 24.0297);
-  Map json = position._toJson(); // {latitude: 49.8397, longitude: 24.0297}
+  Map<String, double> json = position._toJson(); // {latitude: 49.8397, longitude: 24.0297}
 
   // same as above. Creating an instance of extension JsonPosition call "constuctor" of LatLng
-  Map json2 = JsonPosition(new LatLng(49.8397, 24.0297))._toJson();
+  Map<String, double> json2 = JsonPosition(new LatLng(49.8397, 24.0297))._toJson();
 
+  print(json);
   print(jsonEncode(json)); // {"latitude":49.8397,"longitude":24.0297}  JSON FORMAT
   print(jsonEncode(json2));
 }
--- bin/latlong_examp2.dart
+++ bin/latlong_examp2.dart
@@ -1,16 +1,20 @@
 import 'package:latlong2/latlong.dart';
 
-// type aliases
+// json type aliases
 typedef json = Map<String, double>;
 
 extension JsonPosition on LatLng {
-  json toJson() => {'latitude': latitude, 'longitude': longitude};
+  /* json type aliases */
 
-  LatLng fromJson(json data) {
-    return LatLng(data['latitude'] as double, data['longitude'] as double);
+  json _toJson() =>
+      {'latitude': latitude, 'longitude': longitude}; /*  method from Map() to Json format */
+
+  LatLng _fromJson(json data) {
+    return new LatLng(
+        data['latitude'] as double, data['longitude'] as double); /*  method from Json format to */
   }
 
-  void fronJson2(json data) {
+  void fromJsonChange(json data) {
     latitude = data['latitude'] as double;
     longitude = data['longitude'] as double;
   }
@@ -19,4 +23,14 @@ extension JsonPosition on LatLng {
 // import 'latlong_convert.dart';
 
 // 49.8397° N, 24.0297° E
-void main(List<String> args) {}
+void main(List<String> args) {
+  LatLng originPosition = new LatLng(23.333, 44.333);
+  //originPosition._fromJson('latitude': 49.8397,'longitude': 333);
+  json pos = originPosition._toJson();
+  print(pos);
+  print(originPosition);
+
+  LatLng pos2 = originPosition._fromJson(pos);
+
+  print(pos2);
+}
