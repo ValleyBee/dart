d4d06b24c78b8754314aed93af30ecb5e2f7667b
VallenBee <csplus2010.vb@gmail.com>
2022-06-25 14:36:35 +0300

    new file added

---
+++ bin/testFacility.dart
@@ -0,0 +1,9 @@
+abstract class storageFacilitySystem {
+  late String ItemName;
+  late int ItemWeight;
+
+  void addItem() {}
+  int? totalWeight() {}
+}
+
+class Box extends storageFacilitySystem {}
