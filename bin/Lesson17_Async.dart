8e2aa962d868b55853c823751dd993cde162989e
VallenBee <csplus2010.vb@gmail.com>
2022-07-19 07:34:50 +0300

    some changes were added

--- bin/Lesson17_Async.dart
+++ bin/Lesson17_Async.dart
@@ -1,5 +1,6 @@
 // Async
 import 'dart:async';
+//import 'dart:io';
 
 void main(List<String> args) {
   Future<void> user = new Future(() {
@@ -17,4 +18,55 @@ void main(List<String> args) {
   });
 
   print('main');
+
+  first();
+  second();
+  third();
 }
+
+void first() {
+  print("First");
+}
+
+second() async {
+  await four();
+  for (int i = 0; i < 100; i++);
+  print("second");
+}
+
+third() {
+  print('third');
+}
+
+Future four() async {
+  for (int i = 0; i < 10000000; i++);
+  print("four");
+}
+
+/* output
+without await four()
+--------------------
+main
+First 
+four
+second
+third
+- 1st element of microtask
+- 2nd element of microtask
+ - 1st element of event loop
+ - 2nd element of event loop
+
+with await four()
+----------------
+main
+First
+four
+third
+- 1st element of microtask
+- 2nd element of microtask
+second
+ - 1st element of event loop
+ - 2nd element of event loop
+
+
+ */
--- bin/lesson20_async.dart
+++ bin/lesson20_async.dart
@@ -1,5 +1,9 @@
 import 'dart:async';
 import 'dart:math';
+import 'dart:convert';
+import 'dart:io';
+
+import 'lesson21_stream.dart';
 
 class Test1 {}
 
@@ -10,24 +14,50 @@ void main(List<String> args) {
   list.add('ok');
   list.add('Buy');
 
-  List<int> list2 = list.map((e) => e.length).toList();
-  print(list2);
+  void len(int a) {
+    List<int> list2 = list.map((e) => e.length).toList();
+    print(list2);
+  }
+
+  File file = new File('file.txt');
+  Future<String> fileContents = file.readAsString();
+
+  String fileOut() {
+    fileContents.then((String msg) => msg.toString());
+    return 'ok';
+  }
+
+  Stream<String> aa;
+
   List<int> array = [5, 9, 3, 8, 1];
 
   StreamController<String> controllerStr = new StreamController<String>();
   StreamController<int> controllerInt = new StreamController<int>();
+  Stream<int> stInt = controllerInt.stream;
+  Stream<String> stStr = controllerStr.stream;
+//  StreamSubscription subscription = st.listen((item) => print(item)); // This is the Trap
+  StreamSubscription subscription = stInt.listen(len);
+  StreamSubscription subscription2 = stStr.listen((item) => print(item));
 
-  StreamSubscription subscription =
-      controllerStr.stream.listen((item) => print(item + ' ' + item)); // This is the Trap
+  // subscription = controllerStr.stream;
 
-  list.forEach((element) {
-    controllerStr.add(element);
-  });
-  subscription = controllerInt.stream.listen((item) => {
-        if (item >= 5) {print(item)}
-      });
+  // list.forEach((element) {
+
+  // });
+
+  // controllerStr.addError('Oops');
+  // subscription = controllerInt.stream.listen((item) => {
+  //       if (item >= 5) {print(item)}
+  //     });
 
   array.forEach((element) {
-    controllerInt.add(element);
+    controllerInt.sink.add(element);
+    //  controllerInt.add(element);
   });
+
+  subscription.cancel;
+
+  //fileContents.then((String value) => print(value));
+
+//  subscription2.cancel;
 }
